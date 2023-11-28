fd229e9c56c84ebf6c5a5d4ba6aa9946b8553e06
Aniketbobade <bobadeaniket5@gmail.com>
2023-11-28 15:58:53 +0530

    add mail trigger when backend started

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
-const config= require('./src/config/development');
-require('./src/dbConnection/mongoDbConnect');
-const cloudinary= require("./src/config/cloudinary");
+const mailService = require("./src/helper/emailService");
+const serverStartMail = require("./src/mail-template/serverStartMail");
+const config = require("./src/config/development");
+const dbConnect = require("./src/dbConnection/mongoDbConnect");
+const cloudinary = require("./src/config/cloudinary");
 cloudinary.cloudinaryConnect();
-require('./src/routers/app')(app);
-require('./src/routers/routes')(app);
+require("./src/routers/app")(app);
+require("./src/routers/routes")(app);
 
-app.listen(config.PORT, () => console.log('Free Notes app listening on port ' + config.PORT));
+dbConnect()
+  .then(() => {
+    app.listen(config.PORT, () =>
+      console.log("Free Notes app listening on port " + config.PORT)
+    );
+    let currentDate = new Date();
+    let cDay = currentDate.getDate();
+    let cMonth = currentDate.getMonth() + 1;
+    let cYear = currentDate.getFullYear();
+    let cHours = currentDate.getHours();
+    let cMinutes = currentDate.getMinutes();
+    let cSeconds = currentDate.getSeconds();
+    //  mailService.sendInviteMail(
+    //   "bobadeaniket5@gmail.com",
+    //   "sever started",
+    //   `${cDay + "/" + cMonth + "/" + cYear + " " + cHours + ":" + cMinutes + ":" + cSeconds}`
+    // );
+  })
+  .catch((err) => {
+    console.log(err);
+  });
--- backend/src/dbConnection/mongoDbConnect.js
+++ backend/src/dbConnection/mongoDbConnect.js
@@ -1,22 +1,15 @@
 const mongoose = require("mongoose");
 const config = require("../config/development");
 
-mongoose.set("strictQuery", false);
+const mongodbConnect= async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const db = await mongoose.connect(config.MONGO_DB_URL);
+    console.log(`\n MongoDB connected !! DB HOST: ${db.connection.host}`);
+  } catch (error) {
+    console.log("MONGODB connection FAILED ", error);
+    process.exit(1)
+  }
+}
 
-mongoose
-  .connect(config.MONGO_DB_URL,
-    {
-      // useCreateIndex: true,
-      useNewUrlParser: true,
-      // useUnifiedTopology: true,
-      // useFindAndModify: false,
-    }
-  )
-  .then(() => {
-    console.log("Mongodb connected");
-  })
-  .catch((err) => {
-    console.log("Mongodb connection failed", err);
-  });
-
-module.exports = mongoose;
+module.exports = mongodbConnect;
